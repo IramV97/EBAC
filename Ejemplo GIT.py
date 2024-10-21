57044a74666e21648059b4788ac035148d8227ac
IramV97 <iramvaldes97@gmail.com>
2024-10-21 12:24:36 -0600

    Factor 200

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -5,5 +5,6 @@ b = 30
 c = 30 * 10 * 5
 print("Resultado = ", c)
 
-d = c * 100
+# Se cambia factor de 100 a 200 según Junta Control / requerimiento del negocio
+d = c * 200
 print("Resultado total = ", d)
