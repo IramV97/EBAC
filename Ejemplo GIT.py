300ca794301d1939e09c1c5e5b943c5804d9bb1e
IramV97 <iramvaldes97@gmail.com>
2024-10-21 16:12:36 -0600

    Nueva variable "e" y prueba de link con SourceTree

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -1,8 +1,10 @@
 # Prueba de Python
 # Se añadió la variable "c"
+# Nueva variable 
 a = 10
 b = 30
-c = 30 * 10 * 5
+e= 1.1
+c = 30 * 10 * 5 / e
 print("Resultado = ", c)
 
 # Se cambia factor de 100 a 200 según Junta Control / requerimiento del negocio
